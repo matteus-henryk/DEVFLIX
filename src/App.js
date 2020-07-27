04b80891c4ae27732fe1c44f26d88f3609e07fc0
matteus-henryk <matteus.henryk2@gmail.com>
2020-07-27 13:52:15 -0300

    Setup inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './componentes/Menu';
+import BannerMain from './componentes/BannerMain';
+import dadosIniciais from './data/dados_iniciais.json';
+import Carousel from './componentes/Carousel';
+import Footer from './componentes/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background: "#141414" }}>
+      <Menu  />
+      <BannerMain
+      url={dadosIniciais.categorias[0].videos[0].url}
+      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}      
+      videoDescription={"O que é Front-end? Trabalhando na área"}/>
+      <Carousel 
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[0]}/>
+
+      <Carousel 
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[1]}/>
+
+      <Carousel 
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[2]}/>
+
+      <Carousel 
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[3]}/>
+
+      <Carousel 
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[4]}/>
+      <Carousel 
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[5]}/>
+      <Footer 
+      />
     </div>
+
   );
 }
 
