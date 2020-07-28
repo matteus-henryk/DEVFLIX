676c6b8249c77ac1d5510c4c04dc9ccbb249cdb1
matteus-henryk <matteus.henryk2@gmail.com>
2020-07-28 16:12:33 -0300

    commit de final da segunda aula

--- src/componentes/Carousel/components/Slider/index.js
+++ src/componentes/Carousel/components/Slider/index.js
@@ -6,25 +6,48 @@ import styled from 'styled-components';
 const Container = styled.ul`
   padding: 0;
   margin: 0;
+  height: 197px;
+
+
   .slick-prev,
   .slick-next {
     z-index: 50;
     top: 0;
     bottom: 0;
     margin: auto;
-    width: 30px;
-    height: 30px;
+    width: 70px;
+    height: 220px;
     transform: initial;
+    
     &:before {
       font-size: 30px;
+    }
+    .slick-prev{
+      margin-left: 0px;
+      
+    }
+    .slick-next{
+      margin-right: 0px;
     }
   }
   
   .slick-prev {
-    left: 0;
+    left: -30px;
+    background: #141414;
+    box-shadow: 10px 0px 5px #141414,
+                15px 0px 10px #141414,
+                20px 0px 15px #141414,
+                25px 0px 20px rgba(8, 8, 8, 0.6);
+    border-radius: 5px;
   }
   .slick-next {
-    right: 16px;
+    right: 0px;
+    background: #141414;
+    box-shadow: -10px 0px 5px #141414,
+                -15px 0px 10px #141414,
+                -20px 0px 15px #141414,
+                -25px 0px 20px rgba(8, 8, 8, 0.6);
+    border-radius: 5px;
   }
 `;
 
